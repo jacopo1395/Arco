d2baff435c8afde4180d3a54f68e0ac3a9b582dc
Jacopo Carlini <jack1395@hotmail.it>
2021-05-19 20:25:52 +0200

    first commit

---
+++ src/main/java/com/jacopocarlini/arco/ArcoApplication.java
@@ -0,0 +1,14 @@
+package com.jacopocarlini.arco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcoApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/jacopocarlini/arco/ArcoApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.jacopocarlini.arco;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ArcoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
